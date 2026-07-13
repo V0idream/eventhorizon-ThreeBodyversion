7ad0b63a5571f339742768b0032f69357b16acd1
V0idream <V0idream@users.noreply.github.com>
2026-07-13 22:30:35 +0800

    Make macro-electron targetable by defenses

--- Assets/Modules/AppConfiguration/Scripts/Generated/AppConfig.cs
+++ Assets/Modules/AppConfiguration/Scripts/Generated/AppConfig.cs
@@ -3,7 +3,7 @@ public static class AppConfig
     public const string bundleIdentifier = "com.threebody.EventHorizon";
     public const string version = "Alpha 1.10";
     public const int versionCode = 113910;
-    public const int buildNumber = 1799;
+    public const int buildNumber = 1800;
     public const string localizationFile = "Languages";
     public const bool testMode = false;
     public const bool enableCheats = true;
