b58abafc04255f47cbcc04846df980a6a3479ec9
V0idream <248582221+V0idream@users.noreply.github.com>
2026-07-16 17:41:46 +0800

    chore: update Alpha 1.30 build number

--- Assets/Modules/AppConfiguration/Scripts/Generated/AppConfig.cs
+++ Assets/Modules/AppConfiguration/Scripts/Generated/AppConfig.cs
@@ -3,7 +3,7 @@ public static class AppConfig
     public const string bundleIdentifier = "com.threebody.EventHorizon";
     public const string version = "Alpha 1.30";
     public const int versionCode = 113930;
-    public const int buildNumber = 1839;
+    public const int buildNumber = 1840;
     public const string localizationFile = "Languages";
     public const bool testMode = false;
     public const bool enableCheats = true;
